0199fbf5ee7bda2dc9517f583f2f3407e5a942b9
aziis98 <antonio.delucreziis@gmail.com>
2017-10-14 22:19:02 +0200

    Changed things

---
+++ webpack.config.js
@@ -0,0 +1,63 @@
+
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const CleanPlugin = require('clean-webpack-plugin')
+const webpack = require('webpack');
+const path = require('path');
+const glob = require('glob');
+
+const files = glob.sync('./_src/*.@(sass)').concat(['./_src/app.js', './_src/article.js']);
+console.log('Entry files detected: %s', files.map(f => '\n  ' + f).join('') + '\n');
+
+const fileNames = {};
+
+files.forEach(it => {
+    const basename = path.basename(it);
+    fileNames[basename.substring(0, basename.indexOf('.'))] = it;
+});
+
+const config = {
+
+    devServer: {
+        contentBase: __dirname,
+        publicPath: '/_dist/',
+        port: 8080,
+    },
+
+    entry: fileNames,
+    output: {
+        path: path.resolve(__dirname, '_dist'),
+        filename: '[name].bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(txt|html|json)$/,
+                use: 'raw-loader'
+            },
+            {
+                test: /\.js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['env']
+                    }
+                }
+            },
+            {
+                test: /\.sass$/,
+                use: ExtractTextPlugin.extract({
+                    // fallbackLoader: 'style-loader',
+                    use: 'css-loader?url=false!sass-loader'
+                }),
+            }
+        ]
+    },
+    plugins: [
+        // new webpack.optimize.UglifyJsPlugin(),
+        new ExtractTextPlugin('styles.css'),
+        new CleanPlugin(['_dist']),
+    ]
+};
+
+module.exports = config;
